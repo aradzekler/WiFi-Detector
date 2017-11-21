823d072e72584f6355e9f89a463ca876a0766b81
aradzekler <25280958+aradzekler@users.noreply.github.com>
2017-11-21 16:08:19 +0200

    Update ProgramTest.java
    added testProgram() test.

--- src/ProgramTest.java
+++ src/ProgramTest.java
@@ -1,12 +1,31 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+
 import org.junit.jupiter.api.Test;
 
 class ProgramTest {
+	String sourcefile = "C:\\Users\\Arad Zekler\\Desktop\\test folder\\newfile.csv";
+	String source = "C:\\Users\\Arad Zekler\\Desktop\\test folder";
+	
 
 	@Test
-	void testProgram() {
-		fail("Not yet implemented");
+	void testProgram() { // Asserts if the 2 files were created in paths.
+		String kmlFilePath = "C:\\Users\\Arad Zekler\\Desktop\\test folder\\newKml.kml";
+		Program newfile = new Program(sourcefile, sourcefile, source, "");
+		newfile.csvFolderReader();
+		newfile.writeFileKML();
+		
+		assertEquals(true, checkFileExist(sourcefile));
+		assertEquals(true, checkFileExist(kmlFilePath));
+	}
+
+	boolean checkFileExist(String filePath) { // Checking if file exists.
+		File f = new File(filePath);
+		if(f.exists() && !f.isDirectory()) { 
+			return true;
+		}
+		return false;
 	}
 
 	@Test
