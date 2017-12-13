9e5ed30ab85a6872dfae84fee4aec067cbf49d30
aradzekler <25280958+aradzekler@users.noreply.github.com>
2017-12-13 20:07:55 +0200

    Update Algorithms.java
    not ready

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -2,78 +2,150 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Enumeration;
 import java.util.Vector;
 
 import com.opencsv.*; // imported from library.
 
-public class Algorithms {
+public class Algorithms extends csvWriter{
+
+	public Algorithms() {
+		super(sourceFolder, destinationFile);
+	}
+
+	public Algorithms(String mac) {
+		super(sourceFolder, destinationFile);
+		this.mac = mac;
+	}
+
 	// parameters.
-	private final double POWER = 2;
-	private final double NORM = 10000;
+	private static String mac;
+	private final int POWER = 2;
+	private final int NORM = 10000;
 	private final double SIGNALDIFF = 0.4;
-	private final double MINDIFF = 3;
-	private final double NOSIGNAL = -120;
-	private final double DIFFNOSIGNAL = 100;
+	private final int MINDIFF = 3;
+	private final static int NOSIGNAL = -120;
+	private final int DIFFNOSIGNAL = 100;
 
 	/** Find the weighted center point for maximum 3 networks from the same mac.
 	 * @param mac		 desired mac address.
 	 * @param filePath		 path of CSV file.
 	 */
-	public static double[] weightedCenterPointGrade(String mac, String filePath) {
-		final int NUMBER_OF_MACS = 4; // maximum number of mac addresses to store.
+	public static double[] weightedCenterPointGrade() {
 		double latWeightSum = 0;
 		double sigWeightSum = 0;
 		double lonWeightSum = 0;
 		double altWeightSum = 0;
 
-		Vector<String> Vector = new Vector<String>(2, 2); // Create a new Vector.
+		Vector<Vector<String>> Vector = recordsToVector();
+		filterStrongest(Vector);
+		System.out.println("");
+		printVectorOfVectors(Vector);
+
+		// Calculation.
+		double temp = 0;
+
+		for (int i = 0; i < Vector.size(); i++) {
+			for (int j = 0; j < Vector.get(i).size(); j++) {
+				switch(j) {
+				case 0: 
+					temp = Double.parseDouble(Vector.get(i).get(j)); // Calculating weight for lat.
+					latWeightSum += (1/(temp * temp));
+					break;
+				case 1:
+					temp = Double.parseDouble(Vector.get(i).get(j)); // Calculating weight for lon.
+					lonWeightSum += (1/(temp * temp));
+					break;
+				case 2:
+					temp = Double.parseDouble(Vector.get(i).get(j)); // Calculating weight for alt.
+					altWeightSum += (1/(temp * temp));
+					break;
+				case 3:
+					temp = Double.parseDouble(Vector.get(i).get(j)); // Calculating weight for signal.
+					sigWeightSum += (1/(temp * temp));
+					break;
+				}
+			}
+		}
+		double[] array = {latWeightSum, lonWeightSum, altWeightSum, sigWeightSum};
+		return array;
+	}
+
+	private static Vector<Vector<String>> recordsToVector()  {
+		Vector<Vector<String>> vecOfVecs = new Vector<Vector<String>>();
 
 		try (
-				Reader reader = Files.newBufferedReader(Paths.get(filePath));
+				Reader reader = Files.newBufferedReader(Paths.get(getDestinationFile()));
 				CSVReader csvReader = new CSVReader(reader);
 				) {
 			String[] nextRecord; // Store lines of CSV file.
 			while ((nextRecord = csvReader.readNext()) != null) {
 				for (int i = 0; i < nextRecord.length; i++) {
-					int count = 0;
-					if (nextRecord[i].equals(mac) && count < NUMBER_OF_MACS) { // Adding proper elements to vector.
+					if (nextRecord[i].equals(mac)) { // Adding proper elements to vector.
+						Vector<String> Vector = new Vector<String>(4, 4);
 						Vector.addElement(nextRecord[2]);
 						Vector.addElement(nextRecord[3]);
 						Vector.addElement(nextRecord[4]);
 						Vector.addElement(nextRecord[i+2]);
-						count++;
+						vecOfVecs.add(Vector);
 					}
 				}
-			} // End of reading.
-			// Calculation.
-			double temp = 0;
-			for (int i = 0; i < Vector.size(); i += NUMBER_OF_MACS) {
-				temp = Double.parseDouble(Vector.get(i)); // Calculating weight for lat.
-				latWeightSum += (1/(temp * temp));
 			}
-			for (int i = 1; i < Vector.size(); i += NUMBER_OF_MACS) {
-				temp = Double.parseDouble(Vector.get(i)); // Calculating weight for lon.
-				lonWeightSum += (1/(temp * temp));
-			}
-			for (int i = 2; i < Vector.size(); i += NUMBER_OF_MACS) {
-				temp = Double.parseDouble(Vector.get(i)); // Calculating weight for alt.
-				altWeightSum += (1/(temp * temp));
-			}
-			for (int i = 3; i < Vector.size(); i += NUMBER_OF_MACS) { 
-				temp = Double.parseDouble(Vector.get(i)); // Calculating weight for signal.
-				sigWeightSum += (1/(temp * temp));
-			}
-
-			Enumeration<String> vEnum = Vector.elements(); // Printing the vector properly.
-			while(vEnum.hasMoreElements())
-				System.out.print(vEnum.nextElement() + " ");
-			System.out.println();
+			printVectorOfVectors(vecOfVecs); 
 		} catch (IOException e) {
 			System.out.println("File not found.");
 			e.printStackTrace();
 		}
-		double[] array = {latWeightSum, lonWeightSum, altWeightSum, sigWeightSum};
-		return array;
+		return vecOfVecs;
+	}
+
+	private static void printVectorOfVectors(Vector<Vector<String>> v) {
+		String m = "";
+		for (int i = 0; i < v.size(); i++){
+			for (int j = 0; j < v.get(i).size(); j++) {
+				m = v.get(i).get(j);
+				System.out.print(m + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	private static void filterStrongest(Vector<Vector<String>> vector) {
+		int max1 = NOSIGNAL, max2 = NOSIGNAL, max3 = NOSIGNAL;
+
+		for (int i = 0; i < vector.size(); i++) {
+			for (int j = 3; j < vector.get(i).size(); j+=4) {
+				if (Integer.parseInt(vector.get(i).get(j)) > max1) {
+					max3 = max2; max2 = max1; max1 = Integer.parseInt(vector.get(i).get(j));
+				}
+				else if (Integer.parseInt(vector.get(i).get(j)) > max2) {
+					max3 = max2; max2 = Integer.parseInt(vector.get(i).get(j));
+				}
+				else if (Integer.parseInt(vector.get(i).get(j)) > max3) {
+					max3 = Integer.parseInt(vector.get(i).get(j));
+				}
+				else if (Integer.parseInt(vector.get(i).get(j)) < max3) {
+					vector.remove(i);
+				}
+			}
+		}
+			for (int i = 0; i < vector.size(); i++) {
+				if (Integer.parseInt(vector.get(i).lastElement()) < max3) {
+					vector.remove(i);
+					--i;
+				}
+			}
+		}
+
+	private static void algorithm2() {
+		Vector<Vector<String>> strongestVector = recordsToVector();
+		Vector<Vector<String>> vector = recordsToVector();
+		filterStrongest(strongestVector);
+		
+		for (int i = 0; i < strongestVector.size(); i++) {
+			for (int j = 0; j < vector.size(); j++) {
+				
+			}
+		}
+		
 	}
 }
