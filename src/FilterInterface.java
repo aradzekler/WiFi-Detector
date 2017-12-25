1891b1297a87396fe1e8f4c9996cdb6c4c83d24d
aradzekler <25280958+aradzekler@users.noreply.github.com>
2017-12-25 15:30:41 +0200

    Update FilterInterface.java

--- src/FilterInterface.java
+++ src/FilterInterface.java
@@ -1,4 +1,6 @@
-public interface filterInterface {  // interface for filters
+package filters;
+
+public interface FilterInterface {  // interface for filters
 
 	public void writeFileKML(); // function to filter out and write the file.
 }
