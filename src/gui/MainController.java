8d4906dfa9bb8b6fa0b88c7c2be0c4e2d5d29993
aradzekler <25280958+aradzekler@users.noreply.github.com>
2017-12-28 15:23:14 +0200

    Update MainController.java

--- src/gui/MainController.java
+++ src/gui/MainController.java
@@ -6,19 +6,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.VBox;
 
-public class MainController extends VBox { // main.fxml controller.
-	
+public class MainController { // main.fxml controller.
+
 	@FXML private IOTabController IOTabController;
 
-	// constructor for loading fxml.
-	public MainController() {
-		try {
-			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ui/main.fxml"));
-			fxmlLoader.setRoot(this);
-			fxmlLoader.setController(this);
-			fxmlLoader.load();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	@FXML public void initialize() {
+		System.out.println("Application started");
+		IOTabController.init(this);
 	}
 }
