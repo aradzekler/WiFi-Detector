4f763515ec2bc962d3bb771f85a1ac38c8851654
aradzekler <25280958+aradzekler@users.noreply.github.com>
2017-12-28 15:23:35 +0200

    Update MainGUI.java

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -1,6 +1,7 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -8,17 +9,18 @@ import javafx.stage.Stage;
 // MainGUI class responsible for displaying the GUI.
 public class MainGUI extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 
-        // load main.fxml file from controller.
-    	Parent root = new MainController();
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+		// load main.fxml file from controller.
+		Parent root = FXMLLoader.load(getClass().getResource("/ui/main.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("ubiquitous-tribble");
+		primaryStage.show();
+	}
 
-    public static void main(String[] args) {
-        launch(args); 
-    }
+	public static void main(String[] args) {
+		launch(args); 
+	}
 }
