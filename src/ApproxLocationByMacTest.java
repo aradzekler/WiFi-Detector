27d3a59b7a52ef678689c692a95d0d653b250950
dolevh123 <33119992+dolevh123@users.noreply.github.com>
2017-12-29 15:56:48 +0200

    Add files via upload

---
+++ src/ApproxLocationByMacTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ApproxLocationByMacTest {
+
+	void testProgram() { 
+		latWeightSum
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	@Test
+	void ApproxLocationByMacTest()) {
+		fail("Not yet implemented");
+	}
+
+}
