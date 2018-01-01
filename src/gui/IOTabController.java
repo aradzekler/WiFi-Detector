f51a99694c55496e01f933a1edd421a69b36d37e
aradzekler <25280958+aradzekler@users.noreply.github.com>
2018-01-01 18:49:36 +0200

    Update IOTabController.java

--- src/gui/IOTabController.java
+++ src/gui/IOTabController.java
@@ -1,21 +1,23 @@
 package ui;
 
 import utils.csvWriter;
-import java.io.IOException;
+
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import filters.csvToKml;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Window;
 
-public class IOTabController implements Initializable{  // IOTab.fxml controller
+public class IOTabController implements Initializable {  // IOTab.fxml controller
 
 	@FXML private MainController Main;
 	// variables from fxml file to inject.
@@ -24,8 +26,15 @@ public class IOTabController implements Initializable{  // IOTab.fxml controller
 	@FXML private Button sourceFolderPathSubmit;
 	@FXML private TextField csvFileName;
 	@FXML private Button csvFileNameSubmit;
+	@FXML private Button unifyCsvFiles;
 	@FXML private Button deleteExistingData;
 	@FXML private Button convertCsvToKml;
+	@FXML private Button presentData;
+
+	private String csvPathName;
+	private String csvName;
+	private csvToKml kmlFile;
+	private csvWriter csvFile;
 
 	@FXML
 	protected void handleSourceFolderPathSubmit(ActionEvent event) { // handles path submit.
@@ -33,40 +42,112 @@ public class IOTabController implements Initializable{  // IOTab.fxml controller
 			AlertHelper.showAlert(Alert.AlertType.ERROR, "Form Error!", "Please enter path.");
 			return;
 		}
-		AlertHelper.showAlert(Alert.AlertType.CONFIRMATION, "SUCCESS","Path inserted.");
+		setCsvPathName(sourceFolderFullPath.getText());
+		AlertHelper.showAlert(Alert.AlertType.INFORMATION, "SUCCESS","Path inserted.");
 	}
 
 	@FXML 
 	protected void handleSourceFileNameSubmit(ActionEvent event) { // handles name submit.
-		Window owner = csvFileNameSubmit.getScene().getWindow();
 		if (csvFileName.getText().isEmpty()) { // if empty field
 			AlertHelper.showAlert(Alert.AlertType.ERROR, "Form Error!", "Please enter name.");
 			return;
 		}
-		AlertHelper.showAlert(Alert.AlertType.CONFIRMATION, "SUCCESS","Name inserted.");
+		setCsvName(getCsvPathName() + csvFileName.getText() + ".csv");
+		AlertHelper.showAlert(Alert.AlertType.INFORMATION, "SUCCESS","Name inserted.");
 	}
 
-	@FXML
+	@FXML // creates a new csv file when button clicked.
 	protected void handleCsvFileNameSubmit(ActionEvent event) {
+		if (csvPathName.isEmpty() || csvName.isEmpty()) {
+			AlertHelper.showAlert(Alert.AlertType.ERROR, "FAILURE", "One or both fields are empty. \n Please enter "
+					+ "both full folder path and file name.");
+		}
+		csvWriter unifiedCsvFile = new csvWriter(csvPathName, csvName);
+		unifiedCsvFile.csvWriteFile();
+		setCsvFile(unifiedCsvFile);
+		AlertHelper.showAlert(Alert.AlertType.INFORMATION, "SUCCESS","CSV File created.");
+	}
 
+	@FXML  // deletes all data created
+	protected void handleDeleteExistingData(ActionEvent event) {
+		Alert alert = new Alert(AlertType.CONFIRMATION, "This will delete all data written.", ButtonType.OK, ButtonType.CANCEL);
+		alert.showAndWait();
+
+		if (alert.getResult() == ButtonType.OK) { // if pressed ok, delete all data.
+			File file = new File(getCsvName());
+			file.delete();
+			setCsvName(null);
+			setCsvPathName(null);
+			setCsvFile(null);
+			setKmlFile(null);
+		}
+	}
+
+	@FXML // creates a new kml file.
+	protected void handleConvertCsvToKml(ActionEvent event) {
+		File f = new File(getCsvName());
+		if(f.exists() && !f.isDirectory()) { 
+			csvToKml newkml = new csvToKml(getCsvPathName(), getCsvName());
+			newkml.writeFileKML();
+			setKmlFile(newkml);
+		}
+	}
+
+	@FXML // creates a dialog with data.
+	protected void handlePresentData(ActionEvent event) {
+		getCsvFile();
+		String s = ("Number of data lines:" + csvWriter.getDataCount());
+		AlertHelper.showAlert(Alert.AlertType.INFORMATION, "Details", s);
 	}
 
 
 	// class for creating alerts.
-	public static class AlertHelper {
+	private static class AlertHelper {
 		public static void showAlert(Alert.AlertType alertType, String title, String message) {
 			Alert alert = new Alert(alertType);
 			alert.setTitle(title);
 			alert.setHeaderText(null);
 			alert.setContentText(message);
-			alert.showAndWait();
+			alert.show();
 		}
 	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	public String getCsvName() {
+		return csvName;
+	}
+
+	public void setCsvName(String csvName) {
+		this.csvName = csvName;
+	}
+
+	public String getCsvPathName() {
+		return csvPathName;
+	}
+
+	public void setCsvPathName(String csvPathName) {
+		this.csvPathName = csvPathName;
+	}
+
+	public csvToKml getKmlFile() {
+		return kmlFile;
+	}
+
+	public void setKmlFile(csvToKml kmlfile) {
+		this.kmlFile = kmlfile;
+	}
+
+	public csvWriter getCsvFile() {
+		return csvFile;
+	}
+
+	public void setCsvFile(csvWriter csvFile) {
+		this.csvFile = csvFile;
 	}
 
 }
