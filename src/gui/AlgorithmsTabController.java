02eb915dc1c7298150a4ced73d0d1b9d51756dd7
dolevh123 <33119992+dolevh123@users.noreply.github.com>
2018-01-08 21:06:21 +0200

    Update AlgorithmsTabController.java

--- src/gui/AlgorithmsTabController.java
+++ src/gui/AlgorithmsTabController.java
@@ -1,13 +1,12 @@
 package gui;
 
 import utils.csvWriter;
-
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Vector;
 
 import algorithms.ApproxLocationByMac;
-import filters.csvToKml;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,14 +16,18 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
-public class AlgorithmsTabController {
+public class AlgorithmsTabController  implements Initializable {
 	@FXML private MainController Main;
 	// variables from fxml file to insert.
 	@FXML private Pane AlgorithemTab;
 	//algo 1
 	@FXML private TextField MacBox;
 	@FXML private Button InputCheckMacSubmit;
+	@FXML private TextField LonBox;
+	@FXML private TextField LatBox;
+	@FXML private TextField AltBox;
 	//algo2
 	@FXML private TextField InputCheckString; 
 	@FXML private Button InputCheckStringSubmit;
@@ -35,14 +38,13 @@ public class AlgorithmsTabController {
 	@FXML private TextField pSignal;
 	@FXML private Button InputCheckMac3Submit;
 
-	
 	private String MacSource;
 	private String StringSource;
 	private String Mac1Source;
 	private String Mac2Source;
 	private String Mac3Source;
 	private String Signal;
-	
+
 	//Algorithem 1
 	@FXML // submit mac insert
 	protected void handleCheckMacSubmit(ActionEvent event) { // handles mac submit.
@@ -51,7 +53,13 @@ public class AlgorithmsTabController {
 			return;
 		}
 		setMacSource(MacBox.getText());
+		ApproxLocationByMac algo1=new ApproxLocationByMac ();
+		algo1.weightedCenterPointFirstAlgo( getMacSource());
 		AlertHelper.showAlert(Alert.AlertType.INFORMATION, "SUCCESS","Mac inserted.");
+		Vector<Vector<String>> matrix =algo1.getVector();
+		LatBox.setText(matrix.get(0).get(1));
+		LonBox.setText(matrix.get(0).get(2));
+		AltBox.setText(matrix.get(0).get(3));
 	}
 	//Algorithem 2
 	@FXML // submit String  insert 
@@ -70,58 +78,61 @@ public class AlgorithmsTabController {
 			AlertHelper.showAlert(Alert.AlertType.ERROR, "Form Error!", "Please enter Mac.");
 			return;
 		}
-		 setMac1Source(MacBox1.getText());
-		 setMac2Source(MacBox2.getText());
-		 setMac3Source(MacBox3.getText());
-		 setMac3Source(Signal.getText());
+		setMac1Source(MacBox1.getText());
+		setMac2Source(MacBox2.getText());
+		setMac3Source(MacBox3.getText());
+		setMac3Source(pSignal.getText());
 		AlertHelper.showAlert(Alert.AlertType.INFORMATION, "SUCCESS","Data inserted.");
 	}
 	// class for creating alerts.
-		private static class AlertHelper {
-			public static void showAlert(Alert.AlertType alertType, String title, String message) {
-				Alert alert = new Alert(alertType);
-				alert.setTitle(title);
-				alert.setHeaderText(null);
-				alert.setContentText(message);
-				alert.show();
-			}
+	private static class AlertHelper {
+		public static void showAlert(Alert.AlertType alertType, String title, String message) {
+			Alert alert = new Alert(alertType);
+			alert.setTitle(title);
+			alert.setHeaderText(null);
+			alert.setContentText(message);
+			alert.show();
 		}
-		public String getMacSource() {
-			return MacSource;
-		}
-		public void setMacSource(String MacSource) {
-			this.MacSource = MacSource;
-		}
-		public String getStringSource() {
-			return StringSource;
-		}
-		public void setStringSource(String StringSource) {
-			this.StringSource = StringSource;
-		}
-		public String getMac1Source() {
-			return Mac1Source;
-		}
-		public void setMac1Source(String Mac1Source) {
-			this.Mac1Source = Mac1Source;
-		}
-		public String getMac2Source() {
-			return Mac1Source;
-		}
-		public void setMac2Source(String Mac2Source) {
-			this.Mac2Source = Mac2Source;
-		}
-		public String getMac3Source() {
-			return Mac1Source;
-		}
-		public void setMac3Source(String Mac3Source) {
-			this.Mac3Source = Mac3Source;
-		}
-		public String getSignal() {
-			return Mac1Source;
-		}
-		public void setSignal(String Signal) {
-			this.Signal = Signal;
-		}
-			
-		
+	}
+	public String getMacSource() {
+		return MacSource;
+	}
+	public void setMacSource(String MacSource) {
+		this.MacSource = MacSource;
+	}
+	public String getStringSource() {
+		return StringSource;
+	}
+	public void setStringSource(String StringSource) {
+		this.StringSource = StringSource;
+	}
+	public String getMac1Source() {
+		return Mac1Source;
+	}
+	public void setMac1Source(String Mac1Source) {
+		this.Mac1Source = Mac1Source;
+	}
+	public String getMac2Source() {
+		return Mac1Source;
+	}
+	public void setMac2Source(String Mac2Source) {
+		this.Mac2Source = Mac2Source;
+	}
+	public String getMac3Source() {
+		return Mac1Source;
+	}
+	public void setMac3Source(String Mac3Source) {
+		this.Mac3Source = Mac3Source;
+	}
+	public String getSignal() {
+		return Mac1Source;
+	}
+	public void setSignal(String Signal) {
+		this.Signal = Signal;
+	}
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
 }
